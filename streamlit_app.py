123723cc10901e25e8a21141985b2ba6fdd5cd9a
rynoll <122969548+rynoll@users.noreply.github.com>
2023-01-18 08:54:54 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Parents New HealthyDiner')
 
 streamlit.header('breakfast menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-freerange.text('Kale, SPinach & Rocket Smoothie')
+streamlit.text('Kale, SPinach & Rocket Smoothie')
 stremlit.text('Hard-boiled free-range egg')
