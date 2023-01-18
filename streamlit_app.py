61ac9fe503307eb00e17f0af62093dfa4b40ce4a
rynoll <122969548+rynoll@users.noreply.github.com>
2023-01-18 08:54:14 +0000

    Update streamlit_app.py
    added header and menu options

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New HealthyDiner')
+
+streamlit.header('breakfast menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+freerange.text('Kale, SPinach & Rocket Smoothie')
+stremlit.text('Hard-boiled free-range egg')
